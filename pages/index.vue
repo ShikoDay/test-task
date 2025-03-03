8d0c9bcf821bb058eb5e8cf925ab5b0e3ca01381
ShikoDay <Oskin_vlad@bk.ru>
2025-03-03 17:33:30 +0300

    Init project and download scss

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  
+  <div class="container">
+    <h1>Заголовок</h1>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+$primary-color: #42b983;
+
+.container {
+  background-color: $primary-color;
+  padding: 20px;
+  border-radius: 8px;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
+
+<script setup lang="ts">
+
+</script>
